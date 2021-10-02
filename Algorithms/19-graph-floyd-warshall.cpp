79c6070b33d3a353114a2bd22e7c8e95707ffdf7
Mahesh-2801 <91731276+Mahesh-2801@users.noreply.github.com>
2021-10-02 12:38:58 +0530

    Floyd Warshall Algorithm
    Implemented Floyd Warshall Algorithm

---
+++ Algorithms/19-graph-floyd-warshall.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+  
+// #define ordered_set tree<pi, null_type,less<pi>, rb_tree_tag,tree_order_statistics_node_update> 
+
+#define ll long long
+#define ull unsigned ll
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vd vector<double>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define ff first
+#define ss second
+#define pb push_back
+#define vpi vector<pi>
+#define vpll vector<pll>
+#define umap unordered_map
+#define uset unordered_set
+#define all(x) x.begin(),x.end()
+#define fr(i,j,n) for(int i=j;i<n;++i)
+#define rfr(i,j,n) for(int i=j; i>=n;--i)
+#define MOD 1000000007
+#define INF INT_MAX
+#define LINF LONG_LONG_MAX  
+#define mp make_pair
+#define endl "\n"
+#define sz  size()
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+vvll dist;
+int n;
+void floydwarshall(){
+    dist.assign(n,vll(n,LINF));
+    fr(i,0,n){
+        dist[i][i] = 0;
+    }
+    fr(k,0,n){
+        fr(i,0,n){
+            fr(j,0,n){
+                if(dist[i][k]<LINF && dist[k][j]<LINF){
+                    dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+}
+
+void solve(){
+
+}
+
+void pre() {
+    int t = 1;
+    // cin>>t;
+    fr(i,0,t){
+        // cout<<"Case #"<<i+1<<": ";
+        solve();
+        // cout<<solve()<<endl;
+    }
+}
+
+int main() {
+	fastIO; 
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	pre();
+	return 0; 
+	
+}
